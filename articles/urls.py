a95c0c937f93187baee6d5a18231a7898a72667e
zhazgul00 <jazgul09.03@gmail.com>
2024-11-04 02:48:33 +0600

    Auto-assign comment author;add edit/delete options

--- articles/urls.py
+++ articles/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-
-
-from .views import (ArticleListView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView, ArticleCreateView,)
-
+from .views import (
+    ArticleListView, ArticleDetailView, ArticleUpdateView, 
+    ArticleDeleteView, ArticleCreateView, CommentUpdateView, 
+    CommentDeleteView
+)
 
 urlpatterns = [
-    path("<int:pk>/", ArticleDetailView.as_view(), name="article_detail"), 
-    path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"), 
+    path("<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
+    path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"),
     path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
-    path("new/", ArticleCreateView.as_view(), name="article_new"), 
+    path("new/", ArticleCreateView.as_view(), name="article_new"),
     path("", ArticleListView.as_view(), name="article_list"),
+    path("comment/<int:pk>/edit/", CommentUpdateView.as_view(), name="comment_edit"),
+    path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment_delete"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView, DetailView, FormView
 from django.views.generic.detail import SingleObjectMixin
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy, reverse 
-from .models import Article
+from .models import Article, Comment
 from .forms import CommentForm
 from django.views import View 
 
@@ -24,14 +24,48 @@ class CommentPost(SingleObjectMixin, FormView):
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
         return super().post(request, *args, **kwargs)
+    
     def form_valid(self, form):
         comment = form.save(commit=False)
         comment.article = self.object
+        form.instance.author = self.request.user
         comment.save()
         return super().form_valid(form)
+    
     def get_success_url(self):
         article = self.get_object()
         return reverse("article_detail", kwargs={"pk": article.pk})
+    
+
+from django.core.exceptions import PermissionDenied
+import logging
+
+logger = logging.getLogger(__name__)
+
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    fields = ("comment",)
+    template_name = "comments_edit.html"  
+
+    def test_func(self):
+        comment = self.get_object()
+        return comment.author == self.request.user
+
+    def get_success_url(self):
+        return reverse_lazy("article_detail", kwargs={"pk": self.object.article.pk})
+
+
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = "comments_delete.html"  
+
+    def test_func(self):
+        comment = self.get_object()
+        return comment.author == self.request.user
+
+    def get_success_url(self):
+        return reverse_lazy("article_detail", kwargs={"pk": self.object.article.pk})
+
 
 
 
